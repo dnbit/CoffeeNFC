d8f753c4bdf6054347eeb07dbc033498ef343586
fada21 <lukasz.czaplicki@gmail.com>
2015-10-31 19:57:13 +0000

    count updated

--- app/src/main/java/com/dnbitstudio/coffeenfc/MainActivity.java
+++ app/src/main/java/com/dnbitstudio/coffeenfc/MainActivity.java
@@ -4,17 +4,9 @@ import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.parse.LogInCallback;
-import com.parse.ParseAnonymousUtils;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -43,62 +35,47 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        lastParsedNfcTag = NfcMessageParser.parseNfcIntent(getIntent());
-
-        loginParse();
-        if (lastParsedNfcTag != null) {
-            mainText.setText(lastParsedNfcTag);
-            registerPoint(lastParsedNfcTag);
-        } else {
-            mainText.setText("waiting for nfc scan");
-        }
+        ParseHelper.loginParse();
+        final Intent intent = getIntent();
+        handleNfcIntent(intent);
 
         btnRedeem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Redeeme pressed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Redeem pressed", Toast.LENGTH_SHORT).show();
             }
         });
 
-    }
-
-    private void loginParse() {
-        ParseAnonymousUtils.logIn(new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException e) {
-                if (e != null) {
-                    Log.d("MyApp", "Anonymous login failed.");
-                } else {
-                    Log.d("MyApp", "Anonymous user logged in.");
-                }
-            }
-        });
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         setIntent(intent);
+        handleNfcIntent(intent);
+    }
+
+    private void handleNfcIntent(Intent intent) {
         lastParsedNfcTag = NfcMessageParser.parseNfcIntent(intent);
         if (lastParsedNfcTag != null) {
             mainText.setText(lastParsedNfcTag);
-            registerPoint(lastParsedNfcTag);
+            ParseHelper.registerPoint(lastParsedNfcTag, new ParseHelper.OnQueryComplete<String>() {
+                @Override
+                public void onSuccess(String result) {
+                    ParseHelper.countPoint(result, new ParseHelper.OnQueryComplete<Integer>() {
+                        @Override
+                        public void onSuccess(final Integer result) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(MainActivity.this, "Count: " + result, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    });
+                }
+            });
         }
     }
 
-    private void registerPoint(String idUser) {
-        ParseObject points = new ParseObject("Points");
-        points.put("userId", idUser);
-        points.saveInBackground(new SaveCallback() {
-            public void done(ParseException e) {
-                if (e == null) {
-                    // Saved successfully.
-                    Log.d(TAG, "Message saved!");
-                } else {
-                    // The save failed.
-                    Log.d(TAG, "Message error: " + e);
-                }
-            }
-        });
-    }
 }
--- app/src/main/java/com/dnbitstudio/coffeenfc/ParseHelper.java
+++ app/src/main/java/com/dnbitstudio/coffeenfc/ParseHelper.java
@@ -1,8 +1,9 @@
 package com.dnbitstudio.coffeenfc;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 
-import com.parse.FindCallback;
+import com.parse.CountCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
@@ -11,72 +12,61 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
-import java.util.List;
-
 /**
  * Created by jose on 31/10/15.
  */
-public class ParseHelper
-{
+public class ParseHelper {
     private static final String TAG = MainActivity.class.getName();
 
-    protected static void loginParse()
-    {
-        ParseAnonymousUtils.logIn(new LogInCallback()
-        {
+    public static void loginParse() {
+        ParseAnonymousUtils.logIn(new LogInCallback() {
             @Override
-            public void done(ParseUser user, ParseException e)
-            {
-                if (e != null)
-                {
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
                     Log.d("MyApp", "Anonymous login failed.");
-                } else
-                {
+                } else {
                     Log.d("MyApp", "Anonymous user logged in.");
                 }
             }
         });
     }
 
-    protected static void countPoint(final String userId)
-    {
+    public static void countPoint(final String userId, @NonNull final OnQueryComplete<Integer> callback) {
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Points");
         query.whereEqualTo("userId", userId);
-        query.findInBackground(new FindCallback<ParseObject>()
-        {
-            public void done(List<ParseObject> countPoints, ParseException e)
-            {
-                if (e == null)
-                {
-                    Log.d("score", "Your number of point is: " + countPoints.size());
-                } else
-                {
-                    Log.d("score", "Error: " + e.getMessage());
+        query.countInBackground(new CountCallback() {
+            @Override
+            public void done(int count, ParseException e) {
+                if (e == null) {
+                    callback.onSuccess(count);
+                } else {
+                    Log.d(TAG, "Message error: " + e);
                 }
             }
         });
     }
 
-    protected static void registerPoint(final String idUser)
-    {
+    public static void registerPoint(final String idUser, @NonNull final OnQueryComplete<String> callback) {
         ParseObject points = new ParseObject("Points");
         points.put("userId", idUser);
-        points.saveInBackground(new SaveCallback()
-        {
-            public void done(ParseException e)
-            {
-                if (e == null)
-                {
+        points.saveInBackground(new SaveCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
                     // Saved successfully.
                     Log.d(TAG, "Message saved!");
-                    countPoint(idUser);
-                } else
-                {
+                    callback.onSuccess(idUser);
+                } else {
                     // The save failed.
                     Log.d(TAG, "Message error: " + e);
                 }
             }
         });
     }
+
+    public interface OnQueryComplete<T> {
+        void onSuccess(T result);
+        // void onError(); // not yet :p
+    }
+
 
 }
